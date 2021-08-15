1c5bf957eb791f30efb221f0496772859aa6430e
adhimasran <adhicittamasran@gmail.com>
2021-08-16 02:12:07 +0800

    Manage MainActivity

--- app/src/main/java/com/adhi/githubuser/MainActivity.kt
+++ app/src/main/java/com/adhi/githubuser/MainActivity.kt
@@ -2,15 +2,53 @@ package com.adhi.githubuser
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.adhi.githubuser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var list: ArrayList<User> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.rvUsers.setHasFixedSize(true)
+
+        addUser()
+        showRecyclerCardView()
+    }
+
+    private fun addUser() {
+        val profile = resources.obtainTypedArray(R.array.avatar)
+        val name = resources.getStringArray(R.array.name)
+        val username = resources.getStringArray(R.array.username)
+        val location = resources.getStringArray(R.array.location)
+        val repository = resources.getStringArray(R.array.repository)
+        val company = resources.getStringArray(R.array.company)
+        val follower = resources.getStringArray(R.array.followers)
+        val following = resources.getStringArray(R.array.following)
+
+        for (position in username.indices) {
+            val user = User(
+                profile.getResourceId(position, -1),
+                name[position],
+                username[position],
+                location[position],
+                repository[position],
+                company[position],
+                follower[position],
+                following[position]
+            )
+            list.add(user)
+        }
+    }
+
+    private fun showRecyclerCardView() {
+        binding.rvUsers.layoutManager = LinearLayoutManager(this)
+        val cardViewUser = CardViewUser(list)
+        binding.rvUsers.adapter = cardViewUser
     }
 }
