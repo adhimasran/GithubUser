d0f2e90ddd4d5847287758f718faced42b22ff1e
adhimasran <adhicittamasran@gmail.com>
2021-08-17 10:54:37 +0800

    Do revision in splash image
    Renaming splash screen activity from SplashImage to SplashImageActivity and revise the code

--- app/src/main/java/com/adhi/githubuser/SplashImageActivity.kt
+++ app/src/main/java/com/adhi/githubuser/SplashImageActivity.kt
@@ -4,20 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.view.Window
-import android.view.WindowManager
 
-@Suppress("DEPRECATION")
-class SplashImage : AppCompatActivity() {
+class SplashImageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.splash_image)
+        setContentView(R.layout.activity_splash_image)
 
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
-        Handler().postDelayed({
+        Handler(mainLooper).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
