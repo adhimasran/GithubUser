26b80a345b78b30a7d266e438998f30a077212a4
adhimasran <adhicittamasran@gmail.com>
2021-08-17 10:59:54 +0800

    Changing var to val

--- app/src/main/java/com/adhi/githubuser/User.kt
+++ app/src/main/java/com/adhi/githubuser/User.kt
@@ -5,12 +5,12 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class User(
-    var photo: Int,
-    var name: String,
-    var username: String,
-    var location: String,
-    var repository: Int,
-    var company: String,
-    var follower: Int,
-    var following: Int
+    val photo: Int,
+    val name: String,
+    val username: String,
+    val location: String,
+    val repository: Int,
+    val company: String,
+    val follower: Int,
+    val following: Int
 ) : Parcelable
